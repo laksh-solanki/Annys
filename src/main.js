cc442617219088597b73ab5f0b8529f2fc605fdb
One Mark <lakshsolanki848@gmail.com>
2025-09-12 16:45:15 +0530

    annys

--- src/main.js
+++ src/main.js
@@ -9,17 +9,18 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
   apiKey: "AIzaSyBmxnLcizoxCxSqXDnbr6YvwZpkR4-7hRU",
   authDomain: "annys-8eb9d.firebaseapp.com",
+  databaseURL: "https://annys-8eb9d-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "annys-8eb9d",
   storageBucket: "annys-8eb9d.firebasestorage.app",
   messagingSenderId: "903124008619",
-  appId: "1:903124008619:web:0af066d3913bc8cee301ee",
-  measurementId: "G-NQE638BKPJ"
+  appId: "1:903124008619:web:ffd1b423cc8df05ee301ee",
+  measurementId: "G-4976HSY605"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -35,5 +36,6 @@ const vuetify = createVuetify({
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+app.use(analytics)
 
 app.mount('#app')
