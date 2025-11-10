8b64560166903c24e4cc142804afa606af8412d0
Laksh Solanki <lakshsolanki848@gmail.com>
2025-11-10 18:55:18 +0530

    Annys

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify()],
+  plugins: [vue(), vueDevTools(), vuetify()],
   server: {
     allowedHosts: ['annys.onrender.com'],
   },
